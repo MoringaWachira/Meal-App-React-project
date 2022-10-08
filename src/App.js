dbd0b4f635c9a5b0e6c34b48808d8784296253ce
first-name second-name <silvia.wachira@student.moringa.com>
2022-10-08 19:24:43 +0300

    import components,use hooks and remove variables

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
-import { Outlet } from 'react-router-dom';
 import NavBar from './components/NavBar';
-import Recipe from './components/Contact';
-import Search from './components/Search';
+import { Routes, Route } from 'react-router-dom';
 import { useState, useEffect} from 'react';
-import recipes from "./recipes.json"
 import Footer from './components/Footer';
+import About from './components/About';
+import Contact from './components/Contact';
+import Home from './components/Home';
+import recipes from "./recipes.json"
+
+
 
 
 function App() {
   
-
   const [meals,setMeals] = useState([])
   const [time, setTime] = useState({})
 
@@ -28,39 +30,28 @@ function App() {
 
     }
 
-    let welcome;  
-    let date = new Date();  
-    let hour = date.getHours();  
-    let minute = date.getMinutes();  
-
     
-    if (minute < 10) {  
-      minute = "0" + minute;  
-    }  
-    if (hour < 12) {  
-      welcome = "good morning";  
-    } else if (hour < 17) {  
-      welcome = "good afternoon";  
-    } else {  
-      welcome = "good evening";  
-    }
 
   return (
+
+
     <div className="App">
             <NavBar />
 
-      {/* <h1 className='heading'>CHAKULA APP</h1> */}
-      <div className='info'>
-        <h4>{welcome.toLocaleUpperCase()}</h4>
-        <p><span>Timezone</span> {time.timezone}</p>
-        <p><span>Day of the week</span> {time.day_of_week}</p>
-        <p> <span>Abbreviation</span> {time.abbreviation}</p>
-      </div>
-   
-      <Search meals={meals}/>
+     <Routes>
+          {/* <Route path='/' exact element={<App/>}> */}
+          <Route exact path="/" element={<Home meals={meals} time={time} />}/>
+          <Route path="/about" element={<About/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+
+          {/* </Route> */}
+    
+   </Routes>
+
+      
       <Footer />
       
-      <Outlet/>
+      
     </div>
   );
 }
